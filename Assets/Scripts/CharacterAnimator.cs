a45312d3d3577e72cec9fc64a7dfa7abde93c034
giji676 <tvtengoxachidze@gmail.com>
2022-06-07 21:53:09 +0100

    Added combat idle animation, changed movement idle call

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class CharacterAnimator : MonoBehaviour
 {
     protected Animator animator;
-    PlayerMotor playerMotor;
 
     protected virtual void Start()
     {
         animator = GetComponent<Animator>();
-        playerMotor = GetComponent<PlayerMotor>();
     }
 
     protected virtual void Update()
     {
-        animator.SetFloat("Velocity X", playerMotor.moveDirection.x);
-        animator.SetFloat("Velocity Z", playerMotor.moveDirection.z);
+
     }
 }
--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class PlayerAnimator : CharacterAnimator
 {
+    PlayerMotor playerMotor;
+
+    int velX = Animator.StringToHash("Velocity X");
+    int velZ = Animator.StringToHash("Velocity Z");
+    int usingCombatItem = Animator.StringToHash("UsingCombatItem");
+
     protected override void Start()
     {
         base.Start();
+        playerMotor = GetComponent<PlayerMotor>();
         EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        animator.SetFloat(velX, playerMotor.moveDirection.x);
+        animator.SetFloat(velZ, playerMotor.moveDirection.z);
     }
 
     void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
@@ -15,19 +29,23 @@ public class PlayerAnimator : CharacterAnimator
         if (newItem != null && newItem.equipSlot == EquipmentSlot.Weapon)
         {
             animator.SetLayerWeight(1, 1);
+            animator.SetBool(usingCombatItem, true);
         }
         else if (newItem == null && oldItem != null && oldItem.equipSlot == EquipmentSlot.Weapon)
         {
             animator.SetLayerWeight(1, 0);
+            animator.SetBool(usingCombatItem, false);
         }
 
         if (newItem != null && newItem.equipSlot == EquipmentSlot.Shield)
         {
             animator.SetLayerWeight(2, 1);
+            animator.SetBool(usingCombatItem, true);
         }
         else if (newItem == null && oldItem != null && oldItem.equipSlot == EquipmentSlot.Shield)
         {
             animator.SetLayerWeight(2, 0);
+            animator.SetBool(usingCombatItem, false);
         }
     }
 }
